ce3f4c02a795c30cec186a05e8aa2e0072b95df6
jonysbaum <jondnussbaum@aol.com>
2025-09-18 15:05:43 -0500

    extended basic_function_call.py to include additional call

--- basic_function_call.py
+++ basic_function_call.py
@@ -5,3 +5,12 @@ def get_minutes_as_hours(orig_minutes):
 
 minutes = float(input())
 print(get_minutes_as_hours(minutes))
+
+def calc_total_inches(num_feet, num_inches):
+
+    total_inches = (12 * feet) + inches
+    return total_inches
+
+feet = int(input())
+inches = int(input())
+print('Total inches:', calc_total_inches(feet, inches))
